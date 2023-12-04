8cb7df1e10e7c4afaae2f1ca0a928dd678a8dd58
smasr <saad.masrur21@gmail.com>
2023-12-04 14:49:45 -0600

    second problem

---
+++ smallEnough.js
@@ -0,0 +1,17 @@
+DESCRIPTION:
+/*You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.*/
+
+
+function smallEnough(a, limit){
+    let sortarr = a.sort((a,b)=> a-b)
+    if(sortarr[a.length-1] <= limit){
+      return true
+    }
+    else{
+      return false
+    }
+    
+  
+  }
