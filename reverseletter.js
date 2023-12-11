68de40d778c686185385f466365a41a7f67334d2
smasr <saad.masrur21@gmail.com>
2023-12-11 14:56:07 -0600

    reverseletter

---
+++ reverseletter.js
@@ -0,0 +1,24 @@
+/*Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string*/
+
+
+
+
+function reverseLetter(str) {
+    //coding and coding..
+    let str2= str.replace(/[0-9]/g, '').replace(/[^a-z0-9]/g, '')
+    console.log(str2)
+  
+    return(str2.split('').reverse().join(''))
+   
+  }
