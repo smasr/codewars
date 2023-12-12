c52fc1d0f3893e21b77746836dc455276dcdfce0
smasr <saad.masrur21@gmail.com>
2023-12-12 14:34:27 -0600

    adding comment

--- evenodd.js
+++ evenodd.js
@@ -1,6 +1,6 @@
 /*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
 
-
+//just have to divide by 2 and see if there is any remainder 
 function evenOrOdd(number) {
 
     if(number%2=== 0){
