59376e28851341784675c36b2330523d3c25fe58
smasr <saad.masrur21@gmail.com>
2024-01-14 11:14:27 -0600

    8kyu

---
+++ suSstr.js
@@ -0,0 +1,6 @@
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+    //convert them to numbers to add then put them as a string
+    return (Number(a)+Number(b)).toString()
+  }
