5b5739a2183e86a956e09cadf4de2929ad1397a3
smasr <saad.masrur21@gmail.com>
2024-01-24 14:00:33 -0600

    8kyu

---
+++ stringToArray.js
@@ -0,0 +1,9 @@
+/*Write a function to split a string and convert it into an array of words.*/
+
+function stringToArray(string){
+
+	// code code code
+  //just use slice to diveive the string the split to seperate 
+  return string.slice().split(" ")
+
+}
